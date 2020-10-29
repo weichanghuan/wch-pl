73343170e5a1b1178b9a1f58339dd510febfabdc
危常焕 <changhuan.wei@huifu.com>
2020-10-29 10:54:19 +0800

    add produce

--- pl-spring-boot-starter/src/main/java/org/pl/spring/config/PusherConfig.java
+++ pl-spring-boot-starter/src/main/java/org/pl/spring/config/PusherConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 
 @Configuration
-@Order(2)
+@Order(3)
 public class PusherConfig {
 
     @Autowired
--- pl-spring-boot-starter/src/main/java/org/pl/spring/config/RegistryConfig.java
+++ pl-spring-boot-starter/src/main/java/org/pl/spring/config/RegistryConfig.java
@@ -5,23 +5,23 @@ import org.pl.mq.api.Producer;
 import org.pl.mq.rocket.impl.RocketMqProducer;
 import org.pl.spring.registry.DefRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 
 @Configuration
-@Order(1)
+@Order(2)
 public class RegistryConfig {
 
     @Autowired
-    private RocketMQConfig rocketMQConfigAutowired;
+    @Qualifier("rocketMqProducer")
+    private Producer producer;
 
     // 默认自带的注册中心
     @Bean("defRegistry")
     public Registry springDefRegistry() {
         DefRegistry defRegistry = new DefRegistry();
-        // 默认注入rocketmq
-        Producer producer=new RocketMqProducer(rocketMQConfigAutowired.getAddresses(),"RocketMqProducer");
         defRegistry.setProducer(producer);
         return defRegistry;
     }
